b78a7d795c946ecbd58f22f08dc4ec0ba04a2b70
Goncalo Mira <goncalomira99@gmail.com>
2020-05-12 20:05:26 +0100

    Better Keyboard Layout

--- src/components/virtual-keyboard/VirtualKeyboard.jsx
+++ src/components/virtual-keyboard/VirtualKeyboard.jsx
@@ -10,44 +10,45 @@ export const VirtualKeyboard = () => {
 
   const keyLetters = ["a", "b", "c", "d", "e", "f", "C", "Back", "OK"];
 
-
   const insertCharacter = (e) => {
-      if (e.keyCode === 13) {
-        if (currentKeyValue < 11 && keyboardValue.length <= 5) {
-          setKeyboardValue((keyboardValue) =>
-            keyboardValue.concat(keyNumbers[currentKeyValue - 1])
-          );
-        } else if (
-          currentKeyValue > 10 &&
-          currentKeyValue < 17 &&
-          keyboardValue.length <= 5
-        ) {
-          setKeyboardValue((keyboardValue) =>
-            keyboardValue.concat(keyLetters[currentKeyValue - 11])
-          );
-        } else if (currentKeyValue === 17 && keyboardValue.length <= 6) {
-          // Key C
-          setKeyboardValue([]);
-        } else if (currentKeyValue === 18 && keyboardValue.length <= 6) {
-          // delete last value
-          let newValue = keyboardValue.slice(0, -1);
-          setKeyboardValue(newValue);
-        }
+    if (e.keyCode === 13) {
+      if (currentKeyValue < 11 && keyboardValue.length <= 5) {
+        setKeyboardValue((keyboardValue) =>
+          keyboardValue.concat(keyNumbers[currentKeyValue - 1])
+        );
+      } else if (
+        currentKeyValue > 10 &&
+        currentKeyValue < 17 &&
+        keyboardValue.length <= 5
+      ) {
+        setKeyboardValue((keyboardValue) =>
+          keyboardValue.concat(keyLetters[currentKeyValue - 11])
+        );
+      } else if (currentKeyValue === 17 && keyboardValue.length <= 6) {
+        // Key C
+        setKeyboardValue([]);
+      } else if (currentKeyValue === 18 && keyboardValue.length <= 6) {
+        // delete last value
+        let newValue = keyboardValue.slice(0, -1);
+        setKeyboardValue(newValue);
       }
+    }
   };
 
   const changeKeys = (e) => {
-      if (e.keyCode === 37 && currentKeyValue > 1) {
-        setCurrentKeyValue(currentKeyValue - 1); // right
-      } else if (e.keyCode === 39 && currentKeyValue < 19) {
-        setCurrentKeyValue(currentKeyValue + 1); // left
-      } else if (e.keyCode === 40 && currentKeyValue < 11) {
-        // down
-        setCurrentKeyValue(currentKeyValue + 10);
-      } else if (e.keyCode === 38 && currentKeyValue > 10) {
-        // up
-        setCurrentKeyValue(currentKeyValue - 10);
-      }
+    if (e.keyCode === 37 && currentKeyValue > 1) {
+      setCurrentKeyValue(currentKeyValue - 1); // right
+    } else if (e.keyCode === 39 && currentKeyValue < 19) {
+      setCurrentKeyValue(currentKeyValue + 1); // left
+    } else if (e.keyCode === 40 && currentKeyValue < 10) {
+      // down
+      setCurrentKeyValue(currentKeyValue + 10);
+    } else if (e.keyCode === 40 && currentKeyValue === 10) {
+      setCurrentKeyValue(19); // When pressing down on 9, it will go to OK
+    } else if (e.keyCode === 38 && currentKeyValue > 10) {
+      // up
+      setCurrentKeyValue(currentKeyValue - 10);
+    }
   };
 
   useEventListener("keydown", changeKeys);
