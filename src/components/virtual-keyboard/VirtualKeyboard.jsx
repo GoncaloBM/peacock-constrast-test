be13280b7e50d89078128ad6c2c59e98a7f9af78
Goncalo Mira <goncalomira99@gmail.com>
2020-05-12 19:26:31 +0100

    Buttons are all working

--- src/components/virtual-keyboard/VirtualKeyboard.jsx
+++ src/components/virtual-keyboard/VirtualKeyboard.jsx
@@ -3,52 +3,53 @@ import useEventListener from "./use-event-listener";
 import "./VirtualKeyboard.css";
 
 export const VirtualKeyboard = () => {
-  const [keyboardVisible, setKeyboardVisible] = useState(true);
   const [keyboardValue, setKeyboardValue] = useState([]);
-  const [currentKeyValue, setCurrentKeyValue] = useState(0);
+  const [currentKeyValue, setCurrentKeyValue] = useState(1);
 
   const keyNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const keyLetters = ["a", "b", "c", "d", "e", "f", "C", "Back", "OK"];
 
-  const toggleKeyboard = (e) => {
-    if (!keyboardVisible && e.keyCode === 37) {
-      setKeyboardVisible(true);
-      setCurrentKeyValue(10);
-    }
-  };
 
   const insertCharacter = (e) => {
-    if (keyboardVisible) {
       if (e.keyCode === 13) {
-        if (currentKeyValue < 11) {
+        if (currentKeyValue < 11 && keyboardValue.length <= 5) {
           setKeyboardValue((keyboardValue) =>
             keyboardValue.concat(keyNumbers[currentKeyValue - 1])
           );
-        } else if (currentKeyValue > 10) {
-            setKeyboardValue((keyboardValue) =>
+        } else if (
+          currentKeyValue > 10 &&
+          currentKeyValue < 17 &&
+          keyboardValue.length <= 5
+        ) {
+          setKeyboardValue((keyboardValue) =>
             keyboardValue.concat(keyLetters[currentKeyValue - 11])
           );
+        } else if (currentKeyValue === 17 && keyboardValue.length <= 6) {
+          // Key C
+          setKeyboardValue([]);
+        } else if (currentKeyValue === 18 && keyboardValue.length <= 6) {
+          // delete last value
+          let newValue = keyboardValue.slice(0, -1);
+          setKeyboardValue(newValue);
         }
       }
-    }
   };
 
   const changeKeys = (e) => {
-    if (keyboardVisible) {
-      if (e.keyCode === 37) {
-        setCurrentKeyValue(currentKeyValue - 1);
-      } else if (e.keyCode === 39) {
-        setCurrentKeyValue(currentKeyValue + 1);
+      if (e.keyCode === 37 && currentKeyValue > 1) {
+        setCurrentKeyValue(currentKeyValue - 1); // right
+      } else if (e.keyCode === 39 && currentKeyValue < 19) {
+        setCurrentKeyValue(currentKeyValue + 1); // left
       } else if (e.keyCode === 40 && currentKeyValue < 11) {
+        // down
         setCurrentKeyValue(currentKeyValue + 10);
       } else if (e.keyCode === 38 && currentKeyValue > 10) {
+        // up
         setCurrentKeyValue(currentKeyValue - 10);
       }
-    }
   };
 
-  //   useEventListener("keydown", toggleKeyboard);
   useEventListener("keydown", changeKeys);
   useEventListener("keydown", insertCharacter);
 
