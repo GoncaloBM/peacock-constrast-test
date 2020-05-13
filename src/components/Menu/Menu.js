3fb6e618c2381cb0baec235a850d1d09d5fa588b
reberti <jreberti@leads4sales.pt>
2020-05-13 19:17:39 +0100

    colors from background and text color picker working, BUG - if ok button is pressed  before inserting all letters/numbers on virtual keyboard function breaks because it can't workout #11f, for example,  as a RGB. We need to force all spaces for characters to be filled

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,47 +2,42 @@ import React, { useState, useEffect } from "react";
 import "./Menu.css";
 import Alphapicker from "../colorpicker/alphapicker";
 import Twitterpicker from "../colorpicker/twitterpicker";
-import {VirtualKeyboard} from "../virtual-keyboard/VirtualKeyboard.jsx";
+import { VirtualKeyboard } from "../virtual-keyboard/VirtualKeyboard.jsx";
 const Menu = (props) => {
   let [indexFocusedItem, setIndexFocusedItem] = useState(0);
   let [showVirtualKeyboard, setShowVirtualKeyboard] = useState(false);
 
-  const exitKeyboard = () =>{
+  const exitKeyboard = () => {
     // console.log('joao')
-    setShowVirtualKeyboard(false)
-  }
-  
+    setShowVirtualKeyboard(false);
+  };
 
   useEffect(() => {
     const keyNavigate = (e) => {
-      if ((e.keyCode === 13 && indexFocusedItem ===1) || (e.keyCode === 13 && indexFocusedItem ===4)) {
-        if(showVirtualKeyboard === false){
-          setShowVirtualKeyboard(true)
+      if (
+        (e.keyCode === 13 && indexFocusedItem === 1) ||
+        (e.keyCode === 13 && indexFocusedItem === 4)
+      ) {
+        if (showVirtualKeyboard === false) {
+          setShowVirtualKeyboard(true);
         }
-        
       }
 
-      if(showVirtualKeyboard){
-      }else{
+      if (showVirtualKeyboard) {
+      } else {
         if (e.keyCode === 38) {
-          
           if (indexFocusedItem > 0) {
             indexFocusedItem--;
-  
-            setIndexFocusedItem(indexFocusedItem);
 
+            setIndexFocusedItem(indexFocusedItem);
           }
         } else if (e.keyCode === 40) {
-        
           if (indexFocusedItem < 8) {
-
             indexFocusedItem++;
             setIndexFocusedItem(indexFocusedItem);
-
           }
         }
       }
-      
     };
     window.addEventListener("keydown", keyNavigate);
     return () => {
@@ -57,9 +52,22 @@ const Menu = (props) => {
           Background
         </li>
         <li className={`${indexFocusedItem === 1 ? "focused" : ""}`}>
-          <Twitterpicker getBackgroundColor={(color)=>props.getBackgroundColor(color)} isKeyboardActive= {showVirtualKeyboard} isActive={indexFocusedItem === 1 ? true : false}/>
-          {showVirtualKeyboard && indexFocusedItem === 1 ? <VirtualKeyboard getBackgroundColor={(color)=>props.getBackgroundColor(color)}
-           exitKeyboard={()=>exitKeyboard()} /> : "" }
+          <Twitterpicker
+            isText={false}
+            previewColor={props.backgroundColor}
+            getColor={(color, isText) => props.getColor(color, isText)}
+            isKeyboardActive={showVirtualKeyboard}
+            isActive={indexFocusedItem === 1 ? true : false}
+          />
+          {showVirtualKeyboard && indexFocusedItem === 1 ? (
+            <VirtualKeyboard
+              isText={false}
+              getColor={(color, isText) => props.getColor(color, isText)}
+              exitKeyboard={() => exitKeyboard()}
+            />
+          ) : (
+            ""
+          )}
         </li>
         <li className={`${indexFocusedItem === 2 ? "focused" : ""}`}>
           <Alphapicker isActive={indexFocusedItem === 2 ? true : false} />
@@ -68,8 +76,21 @@ const Menu = (props) => {
       <ul>
         <li className={`${indexFocusedItem === 3 ? "focused" : ""}`}>Text</li>
         <li className={`${indexFocusedItem === 4 ? "focused" : ""}`}>
-          <Twitterpicker isKeyboardActive= {showVirtualKeyboard} isActive={indexFocusedItem === 4 ? true : false}/>
-          {showVirtualKeyboard && indexFocusedItem ===4 ? <VirtualKeyboard exitKeyboard={()=>exitKeyboard()} /> : "" }
+          <Twitterpicker
+            previewColor={props.colorText}
+            isText={true}
+            isKeyboardActive={showVirtualKeyboard}
+            isActive={indexFocusedItem === 4 ? true : false}
+          />
+          {showVirtualKeyboard && indexFocusedItem === 4 ? (
+            <VirtualKeyboard
+            isText={true}
+            getColor={(color, isText) => props.getColor(color, isText)}
+            exitKeyboard={() => exitKeyboard()}
+            />
+          ) : (
+            ""
+          )}
         </li>
         <li className={`${indexFocusedItem === 5 ? "focused" : ""}`}>
           <Alphapicker isActive={indexFocusedItem === 5 ? true : false} />
