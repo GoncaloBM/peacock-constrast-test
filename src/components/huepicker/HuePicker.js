0bc30abbeabe3003a2798524b975afc98227a2f1
rebertionfire <joaoreberti@gmail.com>
2020-05-16 14:13:34 +0100

    mytheme working

--- src/components/huepicker/HuePicker.js
+++ src/components/huepicker/HuePicker.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import { MuiThemeProvider } from "@material-ui/core/styles";
-import MyTheme from "./MyTheme";
+import {customMuiTheme} from "./MyTheme";
 import "./HuePicker.css";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
@@ -20,7 +20,7 @@ class HuePicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hue: 500,
+      hue: 100,
     };
   }
 
@@ -42,54 +42,53 @@ class HuePicker extends Component {
           getHue={(hue) => this.getHue(hue)}
           hue={this.state.hue}
         />
-        <MuiThemeProvider hue={this.state.hue} theme={()=>MyTheme(this.state.hue)}>
+        <MuiThemeProvider theme={customMuiTheme(this.state.hue)}>
           <Button variant="raised">Default</Button>
           <Button
             variant="raised"
-            style={MyTheme.palette.yellow}
+            style={customMuiTheme(this.state.hue).palette.yellow}
             className={classes.primary}
-            hue={this.state.hue}
           >
             Yellow
           </Button>
           <Button
             variant="raised"
-            style={MyTheme.palette.purple}
+            style={customMuiTheme(this.state.hue).palette.purple}
             className={classes.primary}
           >
             Purple
           </Button>
           <Button
             variant="raised"
-            style={MyTheme.palette.orange}
+            style={customMuiTheme(this.state.hue).palette.orange}
             className={classes.primary}
           >
             Orange
           </Button>
           <Button
             variant="raised"
-            style={MyTheme.palette.red}
+            style={customMuiTheme(this.state.hue).palette.red}
             className={classes.primary}
           >
             Red
           </Button>
           {/* <Button
             variant="raised"
-            style={MyTheme.palette.grey}
+            style={customMuiTheme(this.state.hue).palette.grey}
             className={classes.primary}
           >
             Grey
           </Button> */}
           <Button
             variant="raised"
-            style={MyTheme.palette.blue}
+            style={customMuiTheme(this.state.hue).palette.blue}
             className={classes.primary}
           >
             Blue
           </Button>
           <Button
             variant="raised"
-            style={MyTheme.palette.green}
+            style={customMuiTheme(this.state.hue).palette.green}
             className={classes.primary}
           >
             Green
--- src/components/huepicker/MyTheme.js
+++ src/components/huepicker/MyTheme.js
@@ -1,40 +1,49 @@
 import { createMuiTheme } from '@material-ui/core/';
 import { purple, yellow, orange, red, grey, blue, green } from '@material-ui/core/colors/';
 
-let hue = 500;
+let hue = 100;
 
-export default createMuiTheme({
-    palette: {
-        yellow: {
-            main: '#165788',
-            backgroundColor: yellow[hue],
-            color: '#000',
-        },
-        purple: {
-            main: '#69BE28',
-            backgroundColor: purple[hue],
-            color: '#000',
-        },
-        orange: {
-            backgroundColor: orange[hue],
-            color: '#000',
-        },
-        red: {
-            backgroundColor: red[hue],
-            color: '#000',
-        },
-        grey: {
-            backgroundColor: grey[hue],
-            color: '#000',
-        },
-        blue: {
-            backgroundColor: blue[hue],
-            color: '#000',
-        },
-        green: {
-            backgroundColor: green[hue],
-            color: '#000',
-        },
 
-    },
-});
+
+export  const customMuiTheme= (x) => {
+    let bananas = {
+        palette: {
+            yellow: {
+                main: '#165788',
+                backgroundColor: yellow[x],
+                color: '#000',
+            },
+            purple: {
+                main: '#69BE28',
+                backgroundColor: purple[x],
+                color: '#000',
+            },
+            orange: {
+                backgroundColor: orange[x],
+                color: '#000',
+            },
+            red: {
+                backgroundColor: red[x],
+                color: '#000',
+            },
+            grey: {
+                backgroundColor: grey[x],
+                color: '#000',
+            },
+            blue: {
+                backgroundColor: blue[x],
+                color: '#000',
+            },
+            green: {
+                backgroundColor: green[x],
+                color: '#000',
+            },
+    
+        },
+    
+    }
+    
+
+    return createMuiTheme(bananas)
+
+}
