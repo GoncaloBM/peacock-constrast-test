b52986bd87e060fd31fb5c0186405218e8b6a2c1
andre3992 <andre.regedor@hotmail.com>
2020-05-26 14:41:32 +0100

    deletes comments

--- src/components/huepicker/Slider.js
+++ src/components/huepicker/Slider.js
@@ -9,48 +9,6 @@ const useStyles = makeStyles({
   },
 });
 
-/* const marks = [
-  {
-    value: 50,
-    label: "50",
-  },
-  {
-    value: 100,
-    label: "100",
-  },
-  {
-    value: 200,
-    label: "200",
-  },
-  {
-    value: 300,
-    label: "300",
-  },
-  {
-    value: 400,
-    label: "400",
-  },
-  {
-    value: 500,
-    label: "500",
-  },
-  {
-    value: 600,
-    label: "600",
-  },
-  {
-    value: 700,
-    label: "700",
-  },
-  {
-    value: 800,
-    label: "800",
-  },
-  {
-    value: 900,
-    label: "900",
-  },
-]; */
 
 const hueValues = [
   50,
