ebdfcc612007d98e43983ac3cbf20642e6d12651
andre3992 <andre.regedor@hotmail.com>
2020-05-30 14:14:17 +0100

    added fullscreen on hd

--- src/components/Contrast/Contrast.js
+++ src/components/Contrast/Contrast.js
@@ -1,68 +1,75 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import TextDisplay from "../TextDisplay/TextDisplay";
 import Menu from "../Menu/Menu";
 import { SafeMargin } from "../SafeMargin/SafeMargin";
 import "../../App.css";
-import "./Contrast.css"
-
+import "./Contrast.css";
+import { width } from "@material-ui/system";
 
 const Contrast = ({
-    backgroundColor,
-    colorText,
-    bkImage,
-    returnToNavBar,
-    getColor,
-    imageDB,
-    changeBk,
-    fontSize,
-    fontStyle,
-    textPosition,
-    changeFontSizeState,
-    changeFontStyleState,
-    changeTextPosition,
-    picker,
-    safeMargin,
-  }) => {
+  backgroundColor,
+  colorText,
+  bkImage,
+  returnToNavBar,
+  getColor,
+  imageDB,
+  changeBk,
+  fontSize,
+  fontStyle,
+  textPosition,
+  changeFontSizeState,
+  changeFontStyleState,
+  changeTextPosition,
+  picker,
+  safeMargin,
+  fullscreen,
+}) => {
+  const [lateralBar, setlateralBar] = useState(false);
 
-    const [lateralBar, setlateralBar] = useState(false)
-    
-    useEffect(() => {
-      console.log('mount')
-      return () => {
-        console.log('unmount')
-      }
-    }, [lateralBar])
+  useEffect(() => {
+    console.log("mount");
+    return () => {
+      console.log("unmount");
+    };
+  }, [lateralBar]);
 
-    const handleClick = () => {
-      returnToNavBar(lateralBar)
-      setlateralBar(!lateralBar)
-    }
+  const handleClick = () => {
+    returnToNavBar(lateralBar);
+    setlateralBar(!lateralBar);
+  };
 
-
-    return (
-      <>
-        <div className="wrapper">
-          <div className="wrapper-board">
+  return (
+    <>
+      <div className="wrapper">
+        <div
+          className={!fullscreen ? "wrapper-board" : "wrapper-board-fullscreen"}
+        >
+          <div
+            style={{
+              backgroundColor: `${backgroundColor}`,
+              color: `${colorText}`,
+              backgroundImage: `url(${bkImage})`,
+            }}
+            className="Board"
+          >
             <div
-              style={{
-                backgroundColor: `${backgroundColor}`,
-                color: `${colorText}`,
-                backgroundImage: `url(${bkImage})`,
-              }}
-              className="Board"
+              className={!fullscreen ? "text" : "text-fullscreen"}
+              style={{ color: `${colorText}` }}
             >
               <TextDisplay
                 fontSize={fontSize}
                 textPosition={textPosition}
                 fontStyle={fontStyle}
               />
-              {safeMargin && <SafeMargin />}
             </div>
+            {safeMargin && <SafeMargin />}
           </div>
-          <div onClick={handleClick}>SHOW MENU</div>
-  
-          <div  className={lateralBar ? 'showLateralBar menu' : 'hideLateralBar '}>
-            {lateralBar ?  <Menu
+        </div>
+        <div onClick={handleClick}>SHOW MENU</div>
+
+        <div className={lateralBar ? "showLateralBar menu" : "hideLateralBar "}>
+          {lateralBar ? (
+            <Menu
               backToNavbar={returnToNavBar}
               colorText={colorText}
               backgroundColor={backgroundColor}
@@ -74,13 +81,15 @@ const Contrast = ({
               changeFontStyleState={changeFontStyleState}
               changeTextPosition={changeTextPosition}
               picker={picker}
-            /> : ''}
-           
-          </div>
+            />
+          ) : (
+            ""
+          )}
         </div>
-        <footer className="instructions"> Instructions </footer>
-      </>
-    );
-  };
+      </div>
+      <footer className="instructions"> Instructions </footer>
+    </>
+  );
+};
 
-  export default Contrast
+export default Contrast;
